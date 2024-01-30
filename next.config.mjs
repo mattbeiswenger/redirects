ba029c76fa699f70738ed5c8ed8c634d37d44867
Matt Beiswenger <mattbeis@yahoo.com>
2024-01-29 22:17:21 -0600

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: {
+    "/": "https://www.mattbeiswenger.com",
+    "/insta": "https://www.instagram.com/matt.beiswenger/",
+    "/linkedin": "https://www.linkedin.com/in/mattbeiswenger",
+    "/github": "https://github.com/mattbeiswenger",
+    "/twitter": "https://twitter.com/mattbeiswenger",
+  },
+};
 
 export default nextConfig;
