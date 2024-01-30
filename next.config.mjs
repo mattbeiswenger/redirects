556475d441bea98e49dfc9d6f4811997c98b4603
Matt Beiswenger <mattbeis@yahoo.com>
2024-01-29 22:23:23 -0600

    update redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  redirects: {
-    "/": "https://www.mattbeiswenger.com",
-    "/insta": "https://www.instagram.com/matt.beiswenger/",
-    "/linkedin": "https://www.linkedin.com/in/mattbeiswenger",
-    "/github": "https://github.com/mattbeiswenger",
-    "/twitter": "https://twitter.com/mattbeiswenger",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://www.mattbeiswenger.com",
+        permanent: true,
+      },
+      {
+        source: "/insta",
+        destination: "https://www.instagram.com/matt.beiswenger",
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination: "https://www.linkedin.com/in/mattbeiswenger",
+        permanent: true,
+      },
+      {
+        source: "/github",
+        destination: "https://github.com/mattbeiswenger",
+        permanent: true,
+      },
+      {
+        source: "/twitter",
+        destination: "https://twitter.com/mattbeiswenger",
+        permanent: true,
+      },
+    ];
   },
 };
 
